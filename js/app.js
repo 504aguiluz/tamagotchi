8183d5182a17a6c1b02b0a806ec8459a5ef7dbc4
nazdak504 <aguiluz.matt@gmail.com>
2021-08-04 22:40:35 -0500

    ready to start animating

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 console.log('TAMAGOTCHI!!!')
+// variable declarations
+let nameInput = document.getElementById('name-input')
+let nameOutput = document.getElementById('name-output')
+let hungerText = document.getElementById('hunger')
+let sleepText = document.getElementById('sleepiness')
+let boredomText = document.getElementById('boredom')
+let ageText = document.getElementById('age')
+let hungerNum = document.getElementById('hunger-num').innerText
+let sleepNum = document.getElementById('sleep-num').innerText
+let boredomNum = document.getElementById('boredom-num').innerText
+let ageNum = document.getElementById('age-num').innerText
 
 // Requirements
     // Create a repo for your tomagotchi pet -- done
@@ -7,22 +18,20 @@ console.log('TAMAGOTCHI!!!')
 
     class Tamagotchi {
         constructor(name){
-            this.name = name
-            this.hunger = 1
-            this.sleepiness = 1
-            this.boredom = 1
-            this.age = 0
+            this.name = nameInput.value
+            this.hunger = hungerNum
+            this.sleepiness = sleepNum
+            this.boredom = boredomNum
+            this.age = ageNum
             this.timer = 0
         }
         newTamagotchi(name){
+            // Instatiate your Tomagotchi
             const tamagotchi = new Tamagotchi(name)
             console.log(name)   
         }   
-        // newTamagotchi('dick')
     }
     
-    // Instatiate your Tomagotchi
-
     function clickEat(){
         hungerText.innerText = `hunger: ${Math.max(0,hungerNum--)}`
     }
@@ -37,9 +46,8 @@ console.log('TAMAGOTCHI!!!')
     
     function startGame(){
         // name character
-        newTamagotchi(nameInput.value)
         nameOutput.innerHTML = `name: ${nameInput.value}`
-        // timer code with conditinals
+        // timer with conditinals
         let timer = 0
         const intervalID = setInterval(() => {
             timer++
@@ -52,7 +60,7 @@ console.log('TAMAGOTCHI!!!')
                 // // increment sleepiness every 3s
                 sleepText.innerText = `sleepiness: ${Math.max(0,sleepNum++)}`
             } if (timer % 10 === 0){
-                // // increment age every 5s
+                // // increment age every 10s
                 ageText.innerText = `age: ${Math.max(0,ageNum++)}`
             } if (hungerNum > 10 || sleepNum > 10 || boredomNum > 10){
                 clearInterval(intervalID)
@@ -62,15 +70,12 @@ console.log('TAMAGOTCHI!!!')
 
     }
 
-    // Add the ability to name your pet.git
+    // event listeners
+    document.getElementById('eat').addEventListener('click', clickEat)
+    document.getElementById('sleep').addEventListener('click', clickSleep)
+    document.getElementById('play').addEventListener('click', clickPlay)
+    document.getElementById('start').addEventListener('click', startGame)
     
-    // Style the page.
-    
-    // Increase your pet's age every x minutes
-    
-    // Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
-    
-    // You pet should die if Hunger, Boredom, or Sleepiness hits 10.
     
     // Morph your pet at certain ages.
     
@@ -81,33 +86,3 @@ console.log('TAMAGOTCHI!!!')
     // ...with special powers (extend the class)!
     // Add an excercise() method to your tomagotchi, that affects certain properties
     // Add anything you can think of... use your imagination!
-    
-    // variable declarations
-    let nameInput = document.getElementById('name-input')
-    let nameOutput = document.getElementById('name-output')
-    let hungerText = document.getElementById('hunger')
-    let sleepText = document.getElementById('sleepiness')
-    let boredomText = document.getElementById('boredom')
-    let ageText = document.getElementById('age')
-    let hungerNum = document.getElementById('hunger-num').innerText
-    let sleepNum = document.getElementById('sleep-num').innerText
-    let boredomNum = document.getElementById('boredom-num').innerText
-    let ageNum = document.getElementById('age-num').innerText
-    
-    
-    
-    // event listeners
-    document.getElementById('eat').addEventListener('click', clickEat)
-    document.getElementById('sleep').addEventListener('click', clickSleep)
-    document.getElementById('play').addEventListener('click', clickPlay)
-    document.getElementById('start').addEventListener('click', startGame)
-    // document.getElementById('submit').addEventListener('click', getName)
-
-    // Display a character of your choice on the screen to represent your pet --done
-// Display the following metrics for your pet: --done
-    // Hunger (1-10 scale)
-    // Sleepiness (1-10 scale)
-    // Boredom (1-10 scale)
-    // Age
-    
-    // Add buttons to the screen to feed your pet, turn off the lights, and play with your pet.
